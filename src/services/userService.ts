4a3f2fccdd07e42aedfb45993316a3ce42fa1eb6
duribegl <110846568+duribegl@users.noreply.github.com>
2024-05-23 13:11:24 -0500

    Update user service for login

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,12 +1,17 @@
-import  UserLogin from '../db/models/userLogin'
+import UserLogin from "../db/models/userLogin";
 
-export const saveUserLogin = async (email: string, otpCode: string, messageId: string) => {
+export const saveUserLogin = async (
+  email: string,
+  otpCode: string,
+  messageId: string,
+) => {
   try {
-    const userLogin = new UserLogin({email, messageId, otp: otpCode})
-    await userLogin.save()
+    console.log(`saveUserLogin - saving user login for email ${email}`);
+    const userLogin = new UserLogin({ email, messageId, otp: otpCode });
+    await userLogin.save();
   } catch (error) {
-    const errorMessage = `saveEmailOTP - Error saving OTP for email ${email}: ${(error as any).message}`;
+    const errorMessage = `saveUserLogin - Error saving user login ${email}: ${(error as any).message}`;
     console.error(errorMessage);
     throw new Error(errorMessage);
   }
-}
+};
