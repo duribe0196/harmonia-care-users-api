6c8c61fe908ca9d7623b0f0dd2c6e5339bb6b78f
duribegl <110846568+duribegl@users.noreply.github.com>
2024-06-06 14:25:49 -0500

    Find one and update for users

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -7,8 +7,11 @@ export const saveUserLogin = async (
 ) => {
   try {
     console.log(`saveUserLogin - saving user login for email ${email}`);
-    const userLogin = new UserLogin({ email, messageId, otp: otpCode });
-    await userLogin.save();
+    await UserLogin.findOneAndUpdate(
+      { email },
+      { messageId, otp: otpCode },
+      { upsert: true, new: true, setDefaultsOnInsert: true }, // Options
+    );
   } catch (error) {
     const errorMessage = `saveUserLogin - Error saving user login ${email}: ${(error as any).message}`;
     console.error(errorMessage);
